0194e5b14db090d6ca8a3919000327b81856eb97
fab <59584067+FabFromTheSnow@users.noreply.github.com>
2023-04-28 13:37:53 -0400

    Create UrlExtractor.py

---
+++ UrlExtractor.py
@@ -0,0 +1,21 @@
+import requests
+import re
+from bs4 import BeautifulSoup
+
+url = input("Enter the url: ")
+
+if url.startswith("www."):
+    url = "https://" + url
+elif not url.startswith("https://www."):
+    url = "https://www." + url
+
+page = requests.get(url)
+
+oui = str(BeautifulSoup(page.content, 'html.parser'))
+urls = re.findall('(https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+[^\s)\(\"",;*\']*)', oui)
+
+with open('link.txt', "w") as file:
+    for url in urls:
+        file.write(url + '\n')
+
+print("link availalble in link.txt")
